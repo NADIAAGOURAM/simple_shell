58c7ca6a94a96e0f4fe2eb4b2bb8d49cebc28357
NADIAAGOURAM <nadia.agouram@gmail.com>
2023-06-23 10:53:29 -0700

    commit updates

---
+++ main.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+* main - Entry point.
+* @argc: is the number of arguments on the command line.
+* @argv: is an array of arguments on the command line.
+*
+* Return: Always 0.
+*/
+
+int main(int argc, char **argv)
+{
+char *cmdline = NULL;
+char **toks;
+const int RUNNING = 1;
+(void)argc;
+(void)argv;
+
+while (RUNNING)
+{
+if (isatty(STDIN_FILENO))
+/*The isatty returns 1 if the stdin is a terminal and 0 otherwise.*/
+_printf("#cisfun$ ");
+
+cmdline = ReadCmdL();
+if (cmdline == NULL)
+{
+	fprintf(stderr, "Error reading command line.\n");
+	exit(1);
+}
+toks = ToknizeCmdL(cmdline);
+if (toks == NULL)
+{
+	fprintf(stderr, "Error tokenizing command line.\n");
+	free(cmdline);
+	exit(1);
+}
+if (toks[0] != NULL)
+{
+if (!Built_in(toks))
+ExecCmdL(toks);
+}
+
+free_array(toks);
+free(cmdline);
+}
+return (0);
+}
