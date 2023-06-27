f120db49db0caf5053485d68905b75e16fa080fa
uglyswan1 <knza.hrsn.14@gmail.com>
2023-06-27 15:26:26 -0700

    commit updates

--- functions2.c
+++ functions2.c
@@ -21,3 +21,21 @@ break;
 } while (*s++);
 return (num * sign);
 }
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+int len = 0;
+
+while (*s != '\0')
+{
+len++;
+s++;
+}
+return (len);
+}
