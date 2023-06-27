b3f1bb6008c504695890fc5187a963fda3f8b2ee
uglyswan1 <knza.hrsn.14@gmail.com>
2023-06-27 15:28:17 -0700

    commit updates

--- functions2.c
+++ functions2.c
@@ -60,3 +60,26 @@ return (s1[j] - s2[j]);
 }
 return (0);
 }
+
+/**
+ * _strdup - returns a pointer to a new string that duplicate the string str
+ * @str:string to be duplicated
+ * Return:pointer to string or NULL
+ */
+
+char *_strdup(char *str)
+{
+char *a;
+int i, size = 0;
+
+if (str == NULL)
+return (NULL);
+while (str[size] != '\0')
+size++;
+a = malloc(sizeof(char) * size + 1);
+if (a == NULL)
+return (NULL);
+for (i = 0; i < size; i++)
+a[i] = str[i];
+return (a);
+}
